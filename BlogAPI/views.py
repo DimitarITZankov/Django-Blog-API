fc6159357a4b40d79b793b1d7ad399f877825766
Dimitar <dimitarzankovit@gmail.com>
2025-11-17 11:17:40 +0200

    Added permissions so every user can update only its profile

--- BlogAPI/views.py
+++ BlogAPI/views.py
@@ -3,7 +3,11 @@ from BlogAPI import models
 from rest_framework.response import Response
 from rest_framework import status,viewsets
 from BlogAPI import serializers
+from rest_framework.authentication import TokenAuthentication
+from BlogAPI import permissions
 
 class CreateProfileViewSet(viewsets.ModelViewSet):
 	serializer_class = serializers.CreateUserSerializer
 	queryset = models.BlogAPIProfiles.objects.all()
+	authentication_classes = (TokenAuthentication,)
+	permission_classes = (permissions.EditOnlyOwnProifle,)
